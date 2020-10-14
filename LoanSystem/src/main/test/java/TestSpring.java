df2bb348f49ffb07705a76b024e135a7caaef776
603325613 <603325613@qq.com>
2020-10-14 11:54:59 +0800

    2020-10-14-003

--- LoanSystem/src/main/test/java/TestSpring.java
+++ LoanSystem/src/main/test/java/TestSpring.java
@@ -1,25 +1,15 @@
-import com.my.ssm.entity.Clazz;
-import com.my.ssm.entity.Student;
-import com.my.ssm.entity.Teacher;
-import com.my.ssm.service.Insert;
-import com.my.ssm.service.Query;
-import com.my.ssm.service.imp.InsertService;
-import com.my.ssm.service.imp.QueryService;
-import org.junit.Test;
+
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Set;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class TestSpring {
 
     public void test1() throws Exception {
-
+        String s = "123123";
+        int aa=10;
     }
 }
